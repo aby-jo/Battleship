3ba30fd5162431f79c349edc7131acae58c6e907
aby-jo <173944472+aby-jo@users.noreply.github.com>
2025-07-17 22:04:26 +0530

    Refactor folder structure

--- utils/player.js
+++
@@ -1,8 +0,0 @@
-const gameboard = require("./gameboard")
-
-const player=(type)=>{
-    let player_type=type
-    let playergameboard=gameboard()
-    return{player_type,playergameboard}
-}
-module.exports=player
